d7bc048508539960c0847c7205cf4249d73cb255
miyokidev <bgrin@miyoki.ch>
2020-12-09 08:34:03 +0100

    TabBar added, need to find a way to place an icon outside of tabbar on same row

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_eshop_app/models/screen_sizes.dart';
-import '../home_tabbar.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -13,23 +12,86 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  
   @override
   Widget build(BuildContext context) {
     final data = MediaQuery.of(context);
     return DefaultTabController(
-      length: 2,
+      length: 4,
           child: Scaffold(
-        appBar: EShopTabBar("Yo"),
+        appBar: PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: Container(
+        color: Colors.red,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(child: Container()),
+              TabBar(
+                indicatorColor: Colors.white,
+                indicatorSize: TabBarIndicatorSize.label,
+                tabs: [
+                  Text("eS"),
+                  Icon(
+                    Icons.search
+                    ),
+                  Icon(
+                    Icons.favorite_border,
+                  ),
+                  Icon(
+                    Icons.shopping_cart_outlined
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
         backgroundColor: Colors.red,
-        body: Center(
-          child: Text(
-                    "Home " + widget.gender,
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text(
+                    "Home " + widget.gender + " Home",
                     style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: data.size.width * 16 / OPP7ScreenWidth,
                     ),
                   ),
+            ),
+            Center(
+              child: Text(
+                    "Home " + widget.gender + " Search",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: data.size.width * 16 / OPP7ScreenWidth,
+                    ),
+                  ),
+            ),
+            Center(
+              child: Text(
+                    "Home " + widget.gender + " Favorite",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: data.size.width * 16 / OPP7ScreenWidth,
+                    ),
+                  ),
+            ),  
+            Center(
+              child: Text(
+                    "Home " + widget.gender + " Cart",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: data.size.width * 16 / OPP7ScreenWidth,
+                    ),
+                  ),
+            ),
+          ],
         ),
       ),
     );
