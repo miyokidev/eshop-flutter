129322963d466adf118302ac6b9b9b168dcddc50
miyokidev <bgrin@miyoki.ch>
2020-12-09 12:07:10 +0100

    Je sais pas ce qui a changé

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -12,6 +12,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   
   @override
   Widget build(BuildContext context) {
@@ -19,6 +21,47 @@ class _HomePageState extends State<HomePage> {
     return DefaultTabController(
       length: 4,
           child: Scaffold(
+        key: _scaffoldKey,
+        endDrawer: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.red,
+            textTheme: TextTheme(
+              headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold, color: Colors.white),
+              headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic, color: Colors.white),
+              bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Colors.white),
+            ),
+            ),
+          child: Drawer(
+            child: ListView(
+              children: [
+                Ink(
+                  child: ListTile(
+                    onTap: () {
+                      print("1 pressed");
+                  },
+                    title: Text("1"),
+                  ),
+                ),
+                Ink(
+                  child: ListTile(
+                    onTap: () {
+                      print("2 pressed");
+                  },
+                    title: Text("2"),
+                  ),
+                ),
+                Ink(
+                  child: ListTile(
+                    onTap: () {
+                      print("3 pressed");
+                  },
+                    title: Text("3"),
+                  ),
+                ),
+              ],
+            )
+          ),
+        ),
         appBar: PreferredSize(
       preferredSize: Size.fromHeight(kToolbarHeight),
       child: Container(
@@ -27,20 +70,36 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: [
               Expanded(child: Container()),
-              TabBar(
-                indicatorColor: Colors.white,
-                indicatorSize: TabBarIndicatorSize.label,
-                tabs: [
-                  Text("eS"),
-                  Icon(
-                    Icons.search
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TabBar(
+                    isScrollable: true,
+                      indicatorColor: Colors.white,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      tabs: [
+                        Text("eS"),
+                        Icon(
+                          Icons.search
+                          ),
+                        Icon(
+                          Icons.favorite_border,
+                        ),
+                        Icon(
+                          Icons.shopping_cart_outlined
+                        ),
+                      ],
                     ),
-                  Icon(
-                    Icons.favorite_border,
-                  ),
-                  Icon(
-                    Icons.shopping_cart_outlined
-                  ),
+                InkWell(
+                  onTap: () {
+                    _scaffoldKey.currentState.openEndDrawer();
+                  },
+                  child: Icon(
+                    Icons.person_pin,
+                    color: Colors.white,
+                      ),
+                ),
                 ],
               ),
             ],
